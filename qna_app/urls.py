8dbb1da8840e8bf54a3308ac969eddd13a3fbe14
aa-rohan <athapa96@gmail.com>
2019-12-12 17:02:09 +0545

    Added CRUD operations

--- qna_app/urls.py
+++ qna_app/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from qna_app import views
+app_name = 'qna'
 
 urlpatterns = [
-    path('question/', views.questions),
-    path('popular/', views.popular)
+    path('read/', views.questionlist, name='read'),
+    path('popular/', views.popular),
+    path('add/', views.addquestion, name='add'),
+    path('update/<int:id>/', views.update_question, name='update'),
+    path('delete/<int:id>/', views.delete_question, name='delete'),
+    path('vote/<int:id>/', views.vote_question, name='vote_qn'),
+    path('ans/<int:id>/', views.answer, name='answer'),
+    path('listview/', views.QuestionListViews.as_view(), name='qnlist'),
 ]
--- qna_app/views.py
+++ qna_app/views.py
@@ -1,16 +1,89 @@
-from django.shortcuts import render
-from .models import QuestionModel
+from django.shortcuts import render, redirect
+from .models import QuestionModel, CategoryModel, AnswerModel
+from .forms import QuestionForm, AnswerForm
+from django.http import HttpResponse
+from django.views.generic import CreateView, ListView
 
 # Create your views here.
 
 
-def questions(request):
+def questionlist(request):
     questions = QuestionModel.objects.all()
     questions_dict = {'questions': questions}
-    return render(request, 'questions.html', questions_dict)
+    return render(request, 'questionmodel_list.html', questions_dict)
 
 
 def popular(request):
     questions = QuestionModel.objects.filter(question_votes__gt=2)
     questions_dict = {'questions': questions}
     return render(request, 'popular.html', questions_dict)
+
+
+def addquestion(request):
+    if request.method == "POST":
+        form = QuestionForm(request.POST, request.FILES)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('qna:read')
+            except:
+                return HttpResponse('Failed')
+        else:
+            print(form.errors)
+            return HttpResponse('Form not Valid!')
+    else:
+        category = CategoryModel.objects.all
+        return render(request, 'questionmodel_create.html', {'categories': category})
+
+
+def update_question(request, id):
+    question = QuestionModel.objects.get(id=id)
+    if request.method == "POST":
+        form = QuestionForm(request.POST, request.FILES, instance=question)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('qna:read')
+            except:
+                return HttpResponse('Failed')
+        else:
+            print(form.errors)
+            return HttpResponse('Form not Valid!')
+    else:
+        form = QuestionForm(instance=question)
+        return render(request, 'questionmodel_update.html', {'form': form})
+
+
+def delete_question(request, id):
+    question = QuestionModel.objects.get(id=id)
+    question.delete()
+    return redirect('qna:read')
+
+
+def vote_question(request, id):
+    question = QuestionModel.objects.get(id=id)
+    question.question_votes += 1
+    question.save()
+    return redirect('qna:read')
+
+
+def answer(request, id):
+    if request.method == "POST":
+        by = request.POST.get('answered_by')
+        desc = request.POST.get('answer_desc')
+        answer_img = request.POST.get('answer_img')
+        question = QuestionModel.objects.get(id=id)
+        Comment = AnswerModel(answered_by=by, answer_desc=desc,
+                              answer_img=answer_img, question=question)
+        Comment.save()
+        return redirect('qna:read')
+    else:
+        form = AnswerForm
+        a = {'form': form}
+        b = {'id': id}
+        c = {**a, **b}
+        return render(request, 'answermodel_create.html', c)
+
+
+class QuestionListViews(ListView):
+    queryset = QuestionModel.objects.all()
